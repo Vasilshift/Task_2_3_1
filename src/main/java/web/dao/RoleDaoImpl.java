1f20623308d9df07c3de7de031c2c14f6422d4bf
Maxim <maks_m_1998@mail.ru>
2021-07-12 01:48:05 +0300

    Commit

---
+++ src/main/java/web/dao/RoleDaoImpl.java
@@ -0,0 +1,38 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.Role;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class RoleDaoImpl implements RoleDao{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public Role getRoleByName(String name) {
+        return entityManager
+                .createQuery("select r from Role r where r.name = :name", Role.class)
+                .setParameter("name", name)
+                .getSingleResult();
+    }
+
+    @Override
+    public Role getRoleById(int id) {
+        return entityManager.find(Role.class,id);
+    }
+
+    @Override
+    public List<Role> allRoles() {
+        return entityManager.createQuery("From Role", Role.class).getResultList();
+    }
+
+    @Override
+    public Role getDefaultRole() {
+        return getRoleByName("ROLE_USER");
+    }
+}
---
+++ src/main/java/web/service/RoleServiceImpl.java
@@ -0,0 +1,40 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.RoleDao;
+import web.model.Role;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleService{
+
+    @Autowired
+    private RoleDao roleDao;
+
+    @Transactional
+    @Override
+    public Role getRoleByName(String name) {
+        return roleDao.getRoleByName(name);
+    }
+
+    @Transactional(readOnly=true)
+    @Override
+    public Role getRoleById(int id) {
+        return roleDao.getRoleById(id);
+    }
+
+    @Transactional(readOnly=true)
+    @Override
+    public List<Role> allRoles() {
+        return roleDao.allRoles();
+    }
+
+    @Transactional
+    @Override
+    public Role getDefaultRole() {
+        return roleDao.getDefaultRole();
+    }
+}
